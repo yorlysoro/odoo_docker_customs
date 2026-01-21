fe603daaf94db1c6d16d0c3b48d3a3cdf5199134
yorlysoro <yorlysoro@gmail.com>
2026-01-21 16:59:16 -0400

    [FIX] fixed error in call python enviroment in odoo miniconda

--- 19.0/miniconda/odoo_docker_community/entrypoint.sh
+++ 19.0/miniconda/odoo_docker_community/entrypoint.sh
@@ -8,12 +8,12 @@ fi
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db18'}}
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db19'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
 : ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 
-PYTHON_PATH="/opt/odoo/venv/bin/python"
+PYTHON_PATH="/opt/conda/envs/odoo311/bin/python"
 
 DB_ARGS=()
 function check_config() {
